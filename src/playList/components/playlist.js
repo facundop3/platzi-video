1b78f757b92534b6ec3a7210343d077192f67f37
Facundo Petre <fpetre@integraccs.com>
2018-08-27 09:11:24 -0300

    Player icons components

--- src/playList/components/playlist.js
+++ src/playList/components/playlist.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Media from './media'
 import Play from './../../icons/components/play'
+import FullScreen from './../../icons/components/full-screen'
+import Volume from './../../icons/components/volume'
+import Pause from './../../icons/components/pause'
 
 function Playlist (props) {
     const playlist = props.data.categories[0].playlist
@@ -10,8 +13,16 @@ function Playlist (props) {
         Functional component 💁🏻‍♂️
         <Play
           size={60}
-          color="red"
-        />
+          color="red"/>
+        <FullScreen
+          size={80}
+          color="blue"/>
+        <Volume
+        size={100}
+        color="green"/>
+        <Pause 
+        size={30}
+        color= "yellow"/>
         {
           playlist.map(item => <Media {...item} key={item.id}/>)
         }
